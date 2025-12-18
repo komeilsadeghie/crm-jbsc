54c940e75a8a5977046a0ce2c263da4e85b2258b
Kami <komeilsadeghie@gmail.com>
2025-12-18 22:01:09 +0330

    آماده‌سازی برای build در ParsPack

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
+});
