6a0e0bc75bb40831664ef751f1b94ffc555c308f
Kami <komeilsadeghie@gmail.com>
2025-12-18 22:41:26 +0330

    آماده‌سازی برای build در ParsPack

--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -1,16 +1,24 @@
-/// <reference types="react" />
-
-import * as ReactTypes from 'react';
+// This file extends global React namespace for type definitions
+// Import React types directly from react package
+import type { 
+  MouseEvent as ReactMouseEvent,
+  FormEvent as ReactFormEvent,
+  ChangeEvent as ReactChangeEvent,
+  KeyboardEvent as ReactKeyboardEvent,
+  SyntheticEvent as ReactSyntheticEvent,
+  CSSProperties as ReactCSSProperties,
+  ReactNode as ReactReactNode
+} from 'react';
 
 declare global {
   namespace React {
-    type MouseEvent<T = Element> = ReactTypes.MouseEvent<T>;
-    type FormEvent<T = Element> = ReactTypes.FormEvent<T>;
-    type ChangeEvent<T = Element> = ReactTypes.ChangeEvent<T>;
-    type KeyboardEvent<T = Element> = ReactTypes.KeyboardEvent<T>;
-    type SyntheticEvent<T = Element, E = Event> = ReactTypes.SyntheticEvent<T, E>;
-    type CSSProperties = ReactTypes.CSSProperties;
-    type ReactNode = ReactTypes.ReactNode;
+    type MouseEvent<T = Element> = ReactMouseEvent<T>;
+    type FormEvent<T = Element> = ReactFormEvent<T>;
+    type ChangeEvent<T = Element> = ReactChangeEvent<T>;
+    type KeyboardEvent<T = Element> = ReactKeyboardEvent<T>;
+    type SyntheticEvent<T = Element, E = Event> = ReactSyntheticEvent<T, E>;
+    type CSSProperties = ReactCSSProperties;
+    type ReactNode = ReactReactNode;
   }
 }
 
